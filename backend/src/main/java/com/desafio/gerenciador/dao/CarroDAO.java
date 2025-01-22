90b1edbcc72df2df1be20557bb4956c2435da298
guirezen <95105563+guirezen@users.noreply.github.com>
2025-01-22 09:20:44 -0300

    feat: complementação dos DAOs de Carro e Moto
    - Adicionados métodos CRUD (Create, Read, Update, Delete) para Carro e Moto.
    - Incluídas consultas específicas por atributos de Carro (quantidade_portas e tipo_combustivel) e Moto (cilindrada).
    - Remoção centralizada no VeiculoDAO com suporte a ON DELETE CASCADE para exclusão automática de registros relacionados.
    - Código otimizado para reutilização de lógica e melhor legibilidade.

--- backend/src/main/java/com/desafio/gerenciador/dao/CarroDAO.java
+++ backend/src/main/java/com/desafio/gerenciador/dao/CarroDAO.java
@@ -3,10 +3,13 @@ package com.desafio.gerenciador.dao;
 import com.desafio.gerenciador.model.Carro;
 import com.desafio.gerenciador.utils.DatabaseConnection;
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarroDAO {
     public void inserirCarro(Carro carro) {
@@ -52,6 +55,73 @@ public class CarroDAO {
             stmtCarro.setInt(2, carro.getQuantidadePortas());
             stmtCarro.setString(3, carro.getTipoCombustivel());
             stmtCarro.executeUpdate();
+        }
+
+    }
+
+    public List<Carro> consultarTodosCarros () {
+        List<Carro> carros = new ArrayList<>();
+        String sql = """
+            SELECT v.id, v.modelo, v.fabricante, v.ano, v.preco, c.quantidade_portas, c.tipo_combustivel
+            FROM veiculos v
+            JOIN carros c ON v.id = c.veiculo_id
+          """;
+
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Carro carro = new Carro();
+
+                carro.setId(rs.getInt("id"));
+                carro.setModelo(rs.getString("modelo"));
+                carro.setFabricante(rs.getString("fabricante"));
+                carro.setAno(rs.getInt("ano"));
+                carro.setPreco(rs.getDouble("preco"));
+                carro.setQuantidadePortas(rs.getInt("quantidade_portas"));
+                carro.setTipoCombustivel(rs.getString("tipo_combustivel"));
+
+                carros.add(carro);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao tentar obter todos os carros: " + e.getMessage());
+        }
+
+        return carros;
+    }
+
+    public void atualizarCarro(Carro carro) {
+        String sqlVeiculo = """
+                    UPDATE veiculos SET modelo = ?, fabricante = ?, ano = ?, preco = ?
+                    WHERE veiculo_id = ?
+                """;
+        String sqlCarro = """
+                    UPDATE carros SET quantidade_portas = ?, tipo_combustivel = ?
+                    WHERE veiculo_id = ?
+                """;
+
+        try (Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement stmtVeiculo = connection.prepareStatement(sqlVeiculo);
+        PreparedStatement stmtCarro = connection.prepareStatement(sqlCarro)) {
+
+            stmtVeiculo.setString(1, carro.getModelo());
+            stmtVeiculo.setString(2, carro.getFabricante());
+            stmtVeiculo.setInt(3, carro.getAno());
+            stmtVeiculo.setDouble(4, carro.getPreco());
+            stmtVeiculo.setInt(5, carro.getId());
+            stmtVeiculo.executeUpdate();
+
+            stmtCarro.setInt(1, carro.getQuantidadePortas());
+            stmtCarro.setString(2, carro.getTipoCombustivel());
+            stmtCarro.setInt(3, carro.getId());
+            stmtCarro.executeUpdate();
+
+            System.out.println("Carro atualizado com sucesso!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao tentar atualizar o carro: " + e.getMessage());
         }
 
     }
--- backend/src/main/java/com/desafio/gerenciador/dao/MotoDAO.java
+++ backend/src/main/java/com/desafio/gerenciador/dao/MotoDAO.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MotoDAO {
 
@@ -56,5 +58,72 @@ public class MotoDAO {
             stmtMoto.setInt(2, moto.getCilindrada());
             stmtMoto.executeUpdate();
         }
+    }
+
+    public List<Moto> consultarTodasMotos() {
+        List<Moto> motos = new ArrayList<>();
+        String sql = """
+                SELECT v.id, v.modelo, v.fabricante, v.ano, v.preco, m.cilindrada
+                FROM veiculos v 
+                JOIN motos m ON v.id = m.veiculo_id
+                """;
+
+        try(Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Moto moto = new Moto();
+
+                moto.setId(rs.getInt("id"));
+                moto.setModelo(rs.getString("modelo"));
+                moto.setFabricante(rs.getString("fabricante"));
+                moto.setAno(rs.getInt("ano"));
+                moto.setPreco(rs.getDouble("preco"));
+                moto.setCilindrada(rs.getInt("cilindrada"));
+
+                motos.add(moto);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao tentar obter todas as motos: " + e.getMessage());
+        }
+
+        return motos;
+    }
+
+    public void atualizarMotos(Moto moto) {
+        String sqlVeiculo = """
+                UPDATE veiculos SET modelo = ?, fabricante = ?, ano = ?, preco = ?
+                WHERE veiculo_id = ?
+                """;
+        String sqlMoto = """
+                UPDATE moto SET cilindrada = ?
+                WHERE veiculo_id = ?
+                """;
+
+        try (Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement stmtVeiculo = connection.prepareStatement(sqlVeiculo);
+        PreparedStatement stmtMoto = connection.prepareStatement(sqlMoto)) {
+
+            stmtVeiculo.setString(1, moto.getModelo());
+            stmtVeiculo.setString(2, moto.getFabricante());
+            stmtVeiculo.setInt(3, moto.getAno());
+            stmtVeiculo.setDouble(4, moto.getPreco());
+            stmtVeiculo.setInt(5, moto.getId());
+
+            stmtVeiculo.executeUpdate();
+
+            stmtMoto.setInt(1, moto.getCilindrada());
+            stmtMoto.setInt(2, moto.getId());
+
+            stmtMoto.executeUpdate();
+
+            System.out.println("Moto atualizada com sucesso!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao tentar atulizar os dados da moto: " + e.getMessage());
+        }
+
     }
 }
